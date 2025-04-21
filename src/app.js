c9955c3f46d72734c622c8f74206d2c847849974
otazzu <srotazu@gmail.com>
2025-04-21 12:12:06 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import "bootstrap";
 import "./style.css";
 
+const BTN_REFRESH = document.querySelector("#btn-refresh");
+const NUMBER = document.querySelector("#cardContent");
+const DIV_CARD = document.querySelector("#card");
+const SUITS = ['heart', 'club', 'diamond', 'spade'];
+const POKER_NUMBERS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//LOGICA CARGAR CARTAS AL CLICKAR
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+BTN_REFRESH.addEventListener('click', () => {
+
+  NUMBER.innerHTML = POKER_NUMBERS[randomIndex(POKER_NUMBERS)];
+  DIV_CARD.className = "card";
+  DIV_CARD.classList.add(SUITS[randomIndex(SUITS)]);
+
+})
+
+//LOGICA CARGAR CARTAS CADA 10 SEGUNDOS
+
+setInterval(()=>{
+  NUMBER.innerHTML = POKER_NUMBERS[randomIndex(POKER_NUMBERS)];
+  DIV_CARD.className = "card";
+  DIV_CARD.classList.add(SUITS[randomIndex(SUITS)]);
+}, 10000)
+
+const randomIndex = (array) => {
+  return Math.floor(Math.random() * array.length);
+}
